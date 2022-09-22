ec4ad3bf340e2680554cb0fbf76c396cfa573462
Andressa Helena <andressa.h.moreira@avanade.com>
2022-09-22 14:19:40 -0300

    Encapsulamento

--- logica/dist/Funcionario/Funcionario.js
+++ logica/dist/Funcionario/Funcionario.js
@@ -1,14 +1,33 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-class Funcionario {
-    nome = 'Usuario';
-    ri = '132345';
-    cpf = '123.456.789-00';
-    cadFuncionario(parametroNome, parametroRi, parametroCpf) {
-        this.nome = parametroNome;
+const Pessoa_1 = __importDefault(require("../Pessoa"));
+class Funcionario extends Pessoa_1.default {
+    ri;
+    cpf;
+    _salario = 0;
+    isAdmin = true;
+    constructor(parametroNome, parametroIdade, parametroRi, parametroCpf, parametroSalario) {
+        super(parametroNome, parametroIdade);
         this.ri = parametroRi;
         this.cpf = parametroCpf;
-        console.log(`Funcionário ${this.nome} cadastrado com sucesso!`);
+        this.salario = parametroSalario;
+        console.log(`Funcionário ${this.nome} cadastrado com sucesso! Ganha ${this.salario}`);
+    }
+    get salario() {
+        return (this.isAdmin) ? this._salario : 0;
+    }
+    set salario(value) {
+        if (value < 1200) {
+            this._salario = 1200;
+        }
+        else {
+            this._salario = value;
+        }
     }
 }
 exports.default = Funcionario;
+const avanade = new Funcionario('Andressa', 30, '123123', '123.123.123-00', 1000);
+console.log(avanade.nome);
--- logica/src/Funcionario/Funcionario.ts
+++ logica/src/Funcionario/Funcionario.ts
@@ -2,18 +2,33 @@ import Pessoa from "../Pessoa";
 
 class Funcionario extends Pessoa{
 
-    ri:string;
-    cpf:string;
+    ri: string;
+    cpf: string;
+    private _salario: number = 0;
+    isAdmin: boolean = true;
 
-    constructor(parametroNome: string, parametroIdade: number, parametroRi: string, parametroCpf: string){
-        super(parametroNome, parametroIdade)
+    constructor(parametroNome: string, parametroIdade: number, parametroRi: string, parametroCpf:string,parametroSalario: number){
+        super(parametroNome, parametroIdade);
         this.ri = parametroRi;
         this.cpf = parametroCpf;
-        
+        this.salario = parametroSalario;
+        console.log(`Funcionário ${this.nome} cadastrado com sucesso! Ganha ${this.salario}`);
     }
-    cadFuncionario(){
-        console.log(`Funcionário ${this.nome} cadastrado com sucesso!`);
+
+    get salario(){
+        return (this.isAdmin)?this._salario:0;
+    }
+
+    set salario(value:number){
+        if(value < 1200){
+            this._salario = 1200;
+        }else {
+            this._salario = value;
+        }
     }
 }
 
 export default Funcionario
+
+const avanade = new Funcionario('Andressa', 30, '123123','123.123.123-00', 1000);
+console.log(avanade.nome);
